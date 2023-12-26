48b97d3481265455550935897757f1cea085910a
yudhamahardika <nicoulausyudha212@gmail.com>
2023-12-27 05:12:49 +0700

    Revisi App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, } from 'react-native';
+import { BasketContext } from './Context';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <BasketContext>
       <StatusBar style="auto" />
-    </View>
+      <StackNavigator />
+    </BasketContext>
   );
 }
 
